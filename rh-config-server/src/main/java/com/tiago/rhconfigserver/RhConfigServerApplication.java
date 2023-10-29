c0a81c1905ca579d3ec471dc9b1b1c91c36fbe42
Tiago Ramos <108342700+tiagohenriqueramos@users.noreply.github.com>
2023-10-29 20:51:30 -0300

    rh-config-server

---
+++ rh-config-server/src/main/java/com/tiago/rhconfigserver/RhConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.tiago.rhconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class RhConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhConfigServerApplication.class, args);
+	}
+
+}
---
+++ rh-config-server/src/test/java/com/tiago/rhconfigserver/RhConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago.rhconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
