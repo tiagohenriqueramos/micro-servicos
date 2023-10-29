84bbef74e939ef6660fb8dd39d26c2b984ad929f
Tiago Ramos <108342700+tiagohenriqueramos@users.noreply.github.com>
2023-10-29 12:40:20 -0300

    Zuul

---
+++ rh-api-gateway-zull/src/main/java/com/tiago/rhapigatewayzuul/RhApiGatewayZullApplication.java
@@ -0,0 +1,17 @@
+package com.tiago.rhapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class RhApiGatewayZullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhApiGatewayZullApplication.class, args);
+	}
+
+}
---
+++ rh-api-gateway-zull/src/test/java/com/tiago/rhapigatewayzuul/RhApiGatewayZullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago.rhapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhApiGatewayZullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
