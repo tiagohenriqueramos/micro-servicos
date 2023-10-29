d3e29f045176354742c5ebf66aa3f1581dfc771f
Tiago Ramos <108342700+tiagohenriqueramos@users.noreply.github.com>
2023-10-29 08:18:35 -0300

    Projeto rh-eureka

---
+++ rh-eureka-server/src/main/java/com/tiago/rheurekaserver/RhEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.tiago.rheurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhEurekaServerApplication.class, args);
+	}
+
+}
---
+++ rh-eureka-server/src/test/java/com/tiago/rheurekaserver/RhEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago.rheurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
