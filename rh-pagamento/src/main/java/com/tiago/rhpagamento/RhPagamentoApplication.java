514ab3499b9648930a1c9cb9d0e4636af65cf82d
Tiago Ramos <108342700+tiagohenriqueramos@users.noreply.github.com>
2023-10-29 11:22:28 -0300

    portas aleatorias

--- rh-pagamento/src/main/java/com/tiago/rhpagamento/RhPagamentoApplication.java
+++ rh-pagamento/src/main/java/com/tiago/rhpagamento/RhPagamentoApplication.java
@@ -2,10 +2,9 @@ package com.tiago.rhpagamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "rh-trabalhador")
+
 @EnableFeignClients
 @SpringBootApplication
 public class RhPagamentoApplication {
